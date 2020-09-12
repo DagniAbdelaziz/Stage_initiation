c5e832314d2355ddbec413ec7da8f86197759ced
DagniAbdelaziz <dagni.abdelazize@gmail.com>
2020-09-12 10:39:55 +0200

    script Js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,28 @@
 let buttons=Array.from(document.getElementsByClassName("addtocart"))
-
-
+let cartDiv=document.getElementById("cart")
+buttons.forEach(btn=>btn.addEventListener("click",addHandler))
 let cartItems=[]
 function addHandler(e){
     let parent=e.target.parentNode
+    let id=parent.getAttribute("id")
+    if(!cartItems.includes(id)){
+    	cartItems.push(id)
+    	let div=document.createElement("div")
+    	div.setAttribute("id","star"+id)
+    	div.setAttribute("class","star")
+    	parent.appendChild(div)
+    }else{
+    	cartItems=cartItems.filter(e=>id!=e)
+    	let div=document.getElementById("star"+id)
+    	parent.removeChild(div)
+    }
+	cartDiv.innerText=cartItems.length
+	console.log(id)
 
+}
+
+function myFunction() {
+  var para = document.createElement("P");
+  para.innerHTML = "This is a paragraph.";
+  document.getElementById("myDIV").appendChild(para);
 }
